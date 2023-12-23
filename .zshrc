60d5d05711a956f5263c51d32e10ad98111dced2
atxa <altyew44@gmail.com>
2023-12-24 02:17:02 +0500

    zsh

---
+++ .zshrc
@@ -0,0 +1,44 @@
+autoload -Uz compinit promptinit
+compinit
+promptinit
+
+# This will set the default prompt to the walters theme
+prompt walters
+
+zstyle ':completion:*' menu select
+zstyle ':completion::complete:*' gain-privileges 1
+
+autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+[[ -n "${key[Up]}"   ]] && bindkey -- "${key[Up]}"   up-line-or-beginning-search
+[[ -n "${key[Down]}" ]] && bindkey -- "${key[Down]}" down-line-or-beginning-search
+
+
+alias ls="ls --color -F"
+alias ll="ls --color -alFh"
+alias l="ls -CF"
+alias sspnd="systemctl suspend"
+alias ..="cd .."
+alias vim="nvim"
+
+
+
+tpd() {
+    local TPdevice
+    TPdevice=$(xinput | sed -nre '/TouchPad|Touchpad/s/.*id=([0-9]*).*/\1/p')
+    
+    local state
+    state=$(xinput list-props "$TPdevice" | grep "Device Enabled" | grep -o "[01]$")
+
+    if [ "$state" -eq '1' ]; then
+        xinput --disable "$TPdevice" && notify-send -i emblem-nowrite "Touchpad" "Disabled"
+    else
+        xinput --enable "$TPdevice" && notify-send -i emblem-nowrite "Touchpad" "Enabled"
+    fi
+}
+
+pstkys() {
+    xclip -selection clipboard -out | tr \\n \\r | xdotool selectwindow windowfocus type --clearmodifiers --delay 30 --window %@ --file -
+}
+
--- post-install.sh
+++ post-install.sh
@@ -11,7 +11,16 @@ sudo system76-power graphics hybrid
 sudo apt update && sudo apt upgrade -y
 sudo apt autopurge
 
-sudo apt install -y vlc gnome-tweaks xclip python3-pip
+sudo apt install -y vlc gnome-tweaks xclip python3-pip gh
+
+
+
+# JetBrainsMono Nerd Font
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
+sudo mkdir /usr/share/fonts/jetbrains
+sudo unzip JetBrainsMono.zip -d /usr/share/fonts/jetbrains
+fc-cache -fv
+rm JetBrainsMono.zip
 
 # kitty
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
@@ -20,6 +29,7 @@ cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applicatio
 cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
 sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
 sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator ~/.local/bin/kitty 50
 
 # neovim 
 wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
@@ -29,17 +39,16 @@ sudo cp -r nvim-linux64/lib/* /usr/local/lib/
 sudo cp -r nvim-linux64/man/* /usr/local/man/
 sudo cp -r nvim-linux64/share/* /usr/local/share/
 rm -r nvim-linux64
+sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/nvim 50
 
-# JetBrainsMono Nerd Font
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
-sudo mkdir /usr/share/fonts/jetbrains
-sudo unzip JetBrainsMono.zip -d /usr/share/fonts/jetbrains
-fc-cache -fv
-rm JetBrainsMono.zip
-
-# razer 
+# openrazer
 sudo add-apt-repository ppa:openrazer/stable
 sudo apt update
-sudo apt install openrazer-meta
+sudo apt install -y openrazer-meta
 sudo gpasswd -a $USER plugdev
 pip install razer-cli
+
+# fish
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install -y fish
