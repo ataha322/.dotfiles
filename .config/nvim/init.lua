de572c920f4560365ad4efb822378fae0cecfbfd
Ata Altyyev <ata@paid.ai>
2025-09-01 02:44:08 +0500

    nvim treesitter text objects a/i moves

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -322,7 +322,8 @@ require("lazy").setup({
             "nvim-treesitter/nvim-treesitter",
             branch = 'master',
             lazy = false,
-            build = ":TSUpdate"
+            build = ":TSUpdate",
+            dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
         },
         {
             'nvim-telescope/telescope.nvim',
@@ -354,7 +355,7 @@ require 'nvim-treesitter.configs'.setup {
 
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = false,
+    auto_install = true,
 
     highlight = {
         enable = true,
@@ -378,6 +379,39 @@ require 'nvim-treesitter.configs'.setup {
     indent = {
         enable = true
     },
+
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner',
+                ['ac'] = '@class.outer',
+                ['ic'] = '@class.inner',
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+                [']m'] = '@function.outer',
+                [']]'] = '@class.inner',
+            },
+            goto_next_end = {
+                [']M'] = '@function.outer',
+                [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+                ['[m'] = '@function.outer',
+                ['[['] = '@class.inner',
+            },
+            goto_previous_end = {
+                ['[M'] = '@function.outer',
+                ['[]'] = '@class.outer',
+            },
+        },
+    }
 }
 -----------------------------------------------------------------------------------
 
